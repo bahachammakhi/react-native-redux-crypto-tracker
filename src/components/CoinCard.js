79cc143ff65910595f6b8b8d7c0ef23126801818
Baha Chammakhi <bahachammakhi25@gmail.com>
2019-08-11 00:56:55 +0100

    first commit

---
+++ src/components/CoinCard.js
@@ -0,0 +1,108 @@
+import React from "react";
+import {View, Text,StyleSheet, Image} from "react-native";
+import {images} from "../Utils/CoinIcons";
+
+const styles = StyleSheet.create({
+    container:{
+        display : "flex",
+        marginBottom :20,
+        borderBottomColor: "#e5e5e5",
+        borderBottomWidth: 3,
+        padding : 20
+    },
+    upperRow:{
+        display : "flex",
+        flexDirection:"row",
+        marginBottom: 15,
+    },
+    coinSymbol:{
+        marginTop: 10,
+        marginLeft : 20,
+        marginRight: 5 ,
+        fontWeight: "bold"
+    },
+    CoinName:{
+        marginTop: 10,
+        marginLeft : 5 ,
+        marginRight : 20
+    },
+    seperator:{
+        marginTop : 10
+    },
+    coinPrice:{
+        marginTop:10,
+        marginLeft :"auto",
+        marginRight: 10,
+        fontWeight :"bold"
+    },
+    image:{
+      width:   35,
+      height :35
+    },
+    moneySymbol:{
+       fontWeight:"bold" 
+    },
+    statisticsContainer:{
+        display :"flex",
+        borderTopColor: "#fafafa",
+        borderTopWidth :2,
+        padding: 10,
+        flexDirection :"row",
+        justifyContent : "space-around",
+    },
+    percentChangePlus:{
+        color :"#00bfa5",
+        fontWeight:"bold",
+        marginLeft : 5,
+
+    },
+    percentChangeMinus:{
+        color :"#dd2c00",
+        fontWeight:"bold",
+        marginLeft : 5, 
+    }
+})
+const {
+    container,
+    image,
+    moneySymbol,
+    upperRow,
+    coinSymbol,
+    CoinName,
+    coinPrice,
+    statisticsContainer,
+    seperator,
+    percentChangeMinus,
+    percentChangePlus
+} = styles;
+const CoinCard=({symbol, coin_name, price_usd, percent_change_24hr, percent_change_7d})=>{
+    return(
+        <View style={container} >
+            <View style={upperRow}>
+                <Image style={styles.image} source={{uri : images[symbol]}} />
+                <Text style={coinSymbol} >{symbol}</Text>
+                <Text style={seperator} >|</Text>
+                <Text style={CoinName} >{coin_name}</Text>
+                <Text style={coinPrice} >{price_usd} <Text style={moneySymbol}> $ </Text></Text>
+            </View>
+        
+
+         <View style={statisticsContainer}>  
+            <Text style={percent_change_24hr < 0 ? percentChangeMinus : percentChangePlus}>
+                {percent_change_24hr} %
+          
+            
+            </Text>
+            <Text>7D: 
+            <Text style={percent_change_24hr < 0 ? percentChangeMinus : percentChangePlus}>
+                {percent_change_7d} %
+            </Text>
+            
+            </Text>
+            </View>
+        </View>
+
+       
+    )
+}
+export default CoinCard;
